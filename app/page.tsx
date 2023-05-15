3c1d914bdc4d33f3d80d5986e5260f39890dff65
Huntergs16 <samoyhun@msu.edu>
2023-05-15 14:09:34 -0500

    mobile interactivity updates

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="from-emerald-700 to-emerald-200 bg-gradient-conic min-h-screen flex px-36 py-28 font-mono">
-      <div className='border-white flex border-solid border-8 min-h-full w-full bg-transparent p-12 justify-center gap-8'>
-        <div className='w-1/2 text-emerald-950 font-semibold flex flex-col gap-12'>
+    <main className="from-emerald-700 to-emerald-200 bg-gradient-to-tr sm:bg-gradient-conic min-h-screen w-[100vw] flex p-4 sm:px-36 sm:py-28 font-mono">
+      <div className='sm:border-white flex flex-col sm:flex-row sm:border-solid sm:border-8 min-h-full w-full bg-transparent  p-12 justify-center gap-8'>
+        <div className='sm:w-1/2 h-min text-emerald-950 font-semibold flex flex-col gap-12'>
           <span className='flex items-center gap-10 '>
             <p className='text-7xl'>Hunter Samoy</p>
             <div className='group/profile flex items-center gap-6 px-8'>
               <div className="w-40 h-40 rounded-full overflow-hidden bg-gradient-to-tl from-emerald-700 to-emerald-200 z-10">
                 <Image className="w-full h-full object-cover" src={'/headshot-removebg-preview.png'} alt={'Hunter'} width={200} height={200} />
               </div>
-              <div className='flex gap-4 text-lg -translate-x-44 opacity-0 group-hover/profile:translate-x-0 group-hover/profile:opacity-100 transition-all duration-800 ease-in-out'>
+              <div className='flex gap-4 text-lg -translate-x-[11.5rem] opacity-0 group-hover/profile:translate-y-24 sm:group-hover/profile:translate-x-0 group-hover/profile:opacity-100 transition-all duration-800 ease-in-out'>
                 <Link className='cursor-pointer hover:opacity-40' href={'https://www.linkedin.com/in/hunter-samoy/'} target='_blank'>LinkedIn</Link> 
                 <Link className='cursor-pointer hover:opacity-40' href={'https://github.com/Huntergs16'} target='_blank'>Github</Link>
               </div>
@@ -25,9 +25,9 @@ export default function Home() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima ad mollitia. Inventore vero dicta dolores! Adipisci animi in quaerat quasi esse maiores distinctio pariatur quisquam, aperiam dolorem, et sequi!
           </p>
         </div>
-        <div className='w-1/2 h-full flex flex-col justify-center items-center text-emerald-950 group/projects place-self-center'>
-          <div className='flex flex-col items-center group/projects h-2/3 justify-end'>
-            <p className='text-3xl font-extrabold mb-6'>Projects</p>
+        <div className='sm:w-1/2 h-full flex flex-col justify-center items-center text-emerald-950 group/projects place-self-center'>
+          <div className='flex flex-col sm:items-center group/projects sm:h-2/3 justify-end'>
+            <p className='text-4xl text-emerald-200 sm:text-emerald-950 font-extrabold mb-6'>Projects</p>
             <ul className='text-xl flex flex-col gap-4 items-center justify-center'>
                 <li className='font-bold group/tala cursor-pointer'>
                   <p  className='text-2xl group-hover/tala:text-emerald-700'>Tala</p>
